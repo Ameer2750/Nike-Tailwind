120eef7cb71641b130ff7627f6baf2b1c59fd275
Ameer S <72147027+Ameer2750@users.noreply.github.com>
2023-09-29 10:55:28 +0530

    add constants and add navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
   SpecialOffer,
   Subscribe,
   SuperQuality
-} from './sections'
+} from './sections';
+
+import Nav from './components/Nav';
 
 
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
     </section>
